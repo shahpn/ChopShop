dc18bdbd24f94fd7f0dc6793459296f616a05574
Parth Shah <81637217+shahpn@users.noreply.github.com>
2022-04-14 17:35:29 -0400

    Added webpageText.py and began work on html design for webpage. -Parth

--- webpageText.py
+++ webpageText.py
@@ -3,9 +3,20 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    text = "<title> BluPrnt </title>" \
-           "<h1>Welcome To BluPrnt<h1>" \
-           "<h2> Presented to you by PythonPals &#128151 Industries <h2>"
+    text ="<head>"\
+    "<style>"\
+    "h1 {text-align: center;}"\
+    "h2 {text-align: center;}" \
+    "p {text-align: center;}"\
+    "div {text-align: center;}"\
+    "</style>"\
+    "</head>"\
+    "<body>" \
+    "<body style=background-color:powderblue;>" \
+    "<title> BluPrnt </title>" \
+    "<h1>Welcome To BluPrnt<h1>" \
+    "<h2> Presented to you by PythonPals &#128151 Industries <h2>"\
+    "<p> Enter your grocery list in the section below: <p>"
     return text
 
 # @app.route("/<name>")
