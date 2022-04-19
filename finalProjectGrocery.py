77cd2e4579b2fa20ede241f9d1c7b5de51d3e94d
18gampja <18gampja@gmail.com>
2022-04-19 14:41:46 -0400

    Should be optimal, feel free to check.

--- finalProjectGrocery.py
+++ finalProjectGrocery.py
@@ -6,28 +6,27 @@ import heapq
 def divider():
     print("-------------------------")
 
-def genAisleList(groceryList, aisleContents, requiredAisles = ['Entrance'], i = 'Aisle 1'):
-        
+
+def genAisleList(groceryList, aisleContents, requiredAisles=['Entrance'], i='Aisle 1'):
     # Base case, once the list is empty we have our aisles
 
     if len(groceryList) == 0:
         requiredAisles.append('Checkout')
         return requiredAisles
 
-
     # Checks first to see if the chosen aisle has any of the items on our list
 
     if len(set(aisleContents[i]).intersection(set(groceryList))) != 0:
 
         removeItems = set(aisleContents[i]).intersection(set(groceryList))  # List of items to remove from the grocery list
-        requiredAisles.append(i)                                            # Adds the aisle to the list of places we NEED to see bc it's just so pretty and worth the money
-        #temp = list(aisleContents)                                         # This was a dumb test ignore it
+
+        requiredAisles.append(i)  # Adds the aisle to the list of places we NEED to see bc it's just so pretty and worth the money
 
         # We now remove the items from our list that also exist in our current aisle
 
         for item in removeItems:
             groceryList.remove(item)
-        
+
         # If we're at the end of our aisle, we do one last check. If we are at the end, returns our required MUST SEE aisles
         if i != 'Aisle 13':
             index = list(aisleContents)[list(aisleContents).index(i) + 1]
@@ -54,79 +53,69 @@ def genAisleList(groceryList, aisleContents, requiredAisles = ['Entrance'], i = 
 class layout:
 
     def __init__(self, dictionary=None):
+
         if dictionary is None:
             dictionary = []
+
         self.dictionary = dictionary
 
+
     def printVertices(self):
+
         return f"Graph Vertices: {list(self.dictionary.keys())}"
 
+
     def printEdges(self):
+
         aisles = []
+
         for vertex in self.dictionary:
+
             for nextVertex in self.dictionary[vertex]:
-                if {nextVertex, vertex} not in aisles:
-                    aisles.append({vertex, nextVertex})
-        return f"Graph Edges: {aisles}"
+
+                aisles.append([vertex, nextVertex])
+
+        return aisles
+
 
     def printGraph(self):
         print(f"Current Graph: {self.dictionary}")
 
+
     def dijkstra(self, start):
 
-        distances = {vertex: float('infinity')
-            for vertex in self.dictionary.keys()}
-        
+        distances = {vertex: float('infinity') for vertex in self.dictionary.keys()}
+
         distances[start] = 0
 
         pq = [(0, start)]
+
         while len(pq) > 0:
+
             dist, v = heapq.heappop(pq)
 
             if dist > distances[v]:
                 continue
 
             for path, cost in self.dictionary[v].items():
+
                 distance = dist + cost
 
                 if distance < distances[path]:
                     distances[path] = distance
                     heapq.heappush(pq, (distance, path))
-        
+
         return distances
 
-        # if longOutput:
-        #     dists = []
-        #     for vtx in self.dictionary.keys():
-        #         dists.append(
-        #             f"The distance between the {start} and the {vtx} is {distances[vtx]}.")
-        #     return dists
 
-        # else:
-        #     dists = []
-        #     for vtx in self.dictionary.keys():
-        #         dists.append(f"{distances[vtx]}:{vtx}")
-        #     return f"{start}{dists}"
-
-    
-
-        # for reqItem in groceryList:
-        #     for aisle in aisleItems:
-        #         for item in aisleItems[aisle]:
-        #             if item == reqItem:
-        #                 if aisle not in requiredAisles:
-        #                     requiredAisles.append(aisle)
-        #                     return self.genAisleList(groceryList, requiredAisles, index)
-
-
-    def shoppingPath(self, aisles, frontPath = ['Entrance'], backPath = ['Checkout'], finalPath = [], visited = ['Entrance', 'Checkout'], totalCost = 0, check = None):
-
+    def shoppingPath(self, aisles, frontPath=['Entrance'], backPath=['Checkout'], finalPath=[],
+                     visited=['Entrance', 'Checkout'], totalCost=0, check=None):
 
         # Base case, checks to see if the required
         if (len(set(visited).intersection(set(aisles))) == len(aisles)):
-                finalPath = frontPath + backPath
-                return finalPath
-        
+            finalPath = frontPath + backPath
+            return finalPath
+
         # Always reset this otherwise headaches ensue
         minDist = inf
 
@@ -134,14 +123,13 @@ class layout:
         if check == None:
             check = aisles[:]
 
-
         # Use the dijkstra method to find distances from the front of the check list
         travel = self.dijkstra(check[0])
 
         # Now we search for the one closest to our current location
         for i in travel:
 
-             # current = list(travel.keys())[list(travel).index(i)]
+            # current = list(travel.keys())[list(travel).index(i)]
 
             # Skips iteration if we're looking at the node we're already standing on
             if travel[i] == 0:
@@ -153,7 +141,7 @@ class layout:
                 if minDist > travel[i] and i not in visited and i in aisles:
                     dest = i
                     minDist = travel[i]
-            
+
         visited.append(dest)
 
         # Update some stats and remove unnecesary locations
@@ -161,135 +149,59 @@ class layout:
         totalCost += minDist
         check.pop(check.index(dest))
 
-
         # Resest for HEADACHES
         minDist = inf
 
         if (len(set(visited).intersection(set(aisles))) == len(aisles)):
-                finalPath = frontPath + backPath
-                return finalPath
-        
+            finalPath = frontPath + backPath
+            return finalPath
+
         # Use dijkstra again but this time backward, ask me when I've slept why. !!!!REASONING COULD BE DUMB AND NOT RIGHT!!!!
         travel = self.dijkstra(check[-1])
-
 
         # Traverses the aisles backward because I'm quirky. Note: I'm now realizing I probably didn't have to do this.
         # Does the same thing as the first loop, just for the back of the check list.
         for i in reversed(travel):
-
+        
             if travel[i] == 0:
                 continue
-
+        
             else:
-
+        
                 if minDist > travel[i] and i not in visited and i in aisles:
                     dest = i
                     minDist = travel[i]
         
         visited.append(dest)
-
+        
         # Update fun stats
         backPath.insert(0, dest)
         totalCost += minDist
         check.pop(check.index(dest))
 
-
         # ANOTHA ONE
-        return self.shoppingPath(aisles, frontPath, backPath,
-                     finalPath, visited, totalCost, check)
-
-
-        # Iterates through our required super special photogenic aisles
-        # for i in requiredAisles:
-
-        #     # Returns if 
-        #     if (len(frontPath) + len(backPath) == len(requiredAisles)):
-        #         finalPath = frontPath.append(backPath)
-        #         break
-            
-
-        #     else:
-
-        #         minDist = inf
-
-        #         travel = self.dijkstra(requiredAisles[requiredAisles.index(i)])
-
-        #         for i in travel:
-
-        #             # current = list(travel.keys())[list(travel).index(i)]
-
-        #             if travel[i] == 0:
-        #                 continue
-
-        #             else:
-
-        #                 if minDist > travel[i] and i not in visited:
-        #                     dest = i
-        #                     minDist = travel[i]
-
-        #         frontPath.append(dest)
-        #         totalCost += minDist
-
-        #         if (len(frontPath) + len(backPath) == len(requiredAisles)):
-        #             finalPath = frontPath.append(backPath)
-        #             break
-
-        #         minDist = inf
-
-        #         travel = self.dijkstra(requiredAisles[requiredAisles.index(i)])
-
-        #         for i in reversed(travel):
-
-        #             # current = list(travel.keys())[list(travel).index(i)]
-
-        #             if travel[i] == 0:
-        #                 continue
-
-        #             else:
-
-        #                 if minDist > travel[i] and i not in visited:
-        #                     dest = i
-        #                     minDist = travel[i]          
-
-        # return finalPath
-
-
-
-        # dist = self.dijkstra('Aisle 2')
-        # print(dist)
-        # formattedDistances = []
-
-        # for item in dist:
-        #     formattedDistances.append(item[:-1])
-
-        # formattedDistances.sort(key=lambda x: int(x.split()[-1]))
-        # # print(formattedDistances)
-        # closestLocations = []
-
-        # for item in formattedDistances:
-        #     closestLocations.append(item.split(' is')[0].split("and the ", 1)[1])
-        #     print(dist)
+        return self.shoppingPath(aisles, frontPath, backPath, finalPath, visited, totalCost, check)
 
 
 if __name__ == '__main__':
     divider()
 
     aisleLayout = layout({
-       'Entrance': {'Aisle 1': 2, 'Aisle 2': 4, 'Aisle 3': 6, 'Aisle 4': 9, 'Checkout': 8},
-       'Aisle 1': {'Aisle 5': 2, 'Aisle 2': 2, 'Entrance': 2, 'Checkout': 9},
-       'Aisle 2': {'Aisle 1': 2, 'Aisle 6': 2, 'Aisle 3': 2, 'Entrance': 4, 'Checkout': 7},
-       'Aisle 3': {'Aisle 2': 2, 'Aisle 7': 2, 'Aisle 3': 4, 'Entrance': 6, 'Checkout': 5},
-       'Aisle 4': {'Aisle 3': 2, 'Aisle 8': 2, 'Entrance': 9, 'Checkout': 2},
-       'Aisle 5': {'Aisle 1': 2, 'Aisle 6': 2, 'Aisle 9': 2},
-       'Aisle 6': {'Aisle 5': 2, 'Aisle 2': 2, 'Aisle 7': 2, 'Aisle 10': 2},
-       'Aisle 7': {'Aisle 6': 2, 'Aisle 3': 2, 'Aisle 8': 2, 'Aisle 11': 2},
-       'Aisle 8': {'Aisle 7': 2, 'Aisle 4': 2, 'Aisle 12': 2},
-       'Aisle 9': {'Aisle 5': 2, 'Aisle 10': 2, 'Aisle 13': 4},
-       'Aisle 10': {'Aisle 9': 2, 'Aisle 6': 2, 'Aisle 11': 2, 'Aisle 13': 3},
-       'Aisle 11': {'Aisle 10': 2, 'Aisle 7': 2, 'Aisle 12': 2, 'Aisle 13': 3},
-       'Aisle 12': {'Aisle 11': 2, 'Aisle 8': 2, 'Aisle 13': 4},
-       'Aisle 13': {'Aisle 9': 4, 'Aisle 10': 3, 'Aisle 11': 3, 'Aisle 12': 4},
-       'Checkout': {'Entrance': 8, 'Aisle 4': 2, 'Aisle 3': 5, 'Aisle 2': 7}
+        'Entrance': {'Aisle 1': 2, 'Aisle 2': 4, 'Aisle 3': 6, 'Aisle 4': 9, 'Checkout': 8},
+        'Aisle 1': {'Aisle 5': 2, 'Aisle 2': 2, 'Entrance': 2, 'Checkout': 9},
+        'Aisle 2': {'Aisle 1': 2, 'Aisle 6': 2, 'Aisle 3': 2, 'Entrance': 4, 'Checkout': 7},
+        'Aisle 3': {'Aisle 2': 2, 'Aisle 7': 2, 'Aisle 3': 4, 'Entrance': 6, 'Checkout': 5},
+        'Aisle 4': {'Aisle 3': 2, 'Aisle 8': 2, 'Entrance': 9, 'Checkout': 2},
+        'Aisle 5': {'Aisle 1': 2, 'Aisle 6': 2, 'Aisle 9': 2},
+        'Aisle 6': {'Aisle 5': 2, 'Aisle 2': 2, 'Aisle 7': 2, 'Aisle 10': 2},
+        'Aisle 7': {'Aisle 6': 2, 'Aisle 3': 2, 'Aisle 8': 2, 'Aisle 11': 2},
+        'Aisle 8': {'Aisle 7': 2, 'Aisle 4': 2, 'Aisle 12': 2},
+        'Aisle 9': {'Aisle 5': 2, 'Aisle 10': 2, 'Aisle 13': 4},
+        'Aisle 10': {'Aisle 9': 2, 'Aisle 6': 2, 'Aisle 11': 2, 'Aisle 13': 3},
+        'Aisle 11': {'Aisle 10': 2, 'Aisle 7': 2, 'Aisle 12': 2, 'Aisle 13': 3},
+        'Aisle 12': {'Aisle 11': 2, 'Aisle 8': 2, 'Aisle 13': 4},
+        'Aisle 13': {'Aisle 9': 4, 'Aisle 10': 3, 'Aisle 11': 3, 'Aisle 12': 4},
+        'Checkout': {'Entrance': 8, 'Aisle 4': 2, 'Aisle 3': 5, 'Aisle 2': 7}
     })
 
     aisleItems = {
@@ -297,61 +209,35 @@ if __name__ == '__main__':
         'Aisle 2': {'Coke', 'Sprite', 'Dr. Pepper', 'Fanta', 'Root Beer', 'Ginger Ale'},
         'Aisle 3': {'White Bread', 'Rye Bread', 'Wheat Bread', 'Pumpernickel Bread'},
         'Aisle 4': {'Apples', 'Bananas', 'Grapes', 'Oranges', 'Pineapples', 'Carrots', 'Lettuce', 'Celery', 'Cucumbers',
-                   'Asparagus', 'Broccoli', 'Cauliflower'},
+                    'Asparagus', 'Broccoli', 'Cauliflower'},
         'Aisle 5': {'Bottled Water', 'Purified Water', 'Sparkling Water', 'Mineral Water'},
         'Aisle 6': {'Apple Juice', 'Grape Juice', 'Orange Juice', 'Pineapple Juice', 'Prune Juice'},
         'Aisle 7': {'Beans', 'Chicken Noodle Soup', 'Sardines', 'Green Beans', 'Peas', 'Beef Stew'},
         'Aisle 8': {'Saffron', 'Paprika', 'Cayenne', 'Turmeric', 'Thyme', 'Oregano', 'Ginger', 'Cumin', 'Salt',
-                   'Black Pepper'},
+                    'Black Pepper'},
         'Aisle 9': {'Prescription Drugs', 'Nonprescription Drugs', 'Vitamins', 'Minerals'},
         'Aisle 10': {'Shampoo', 'Conditioner', 'Lotion', 'Toothpaste', 'Toothbrushes', 'Soap', 'Deodorant',
-                    'Hairspray'},
+                     'Hairspray'},
         'Aisle 11': {'Candy', 'Chips', 'Chocolate', 'Cookies', 'Popcorn', 'Pretzels'},
         'Aisle 12': {'Milk', 'Cheese', 'Butter', 'Eggs', 'Yogurt'},
         'Aisle 13': {'Ice Cream', 'Frozen Vegetables', 'Frozen Meats', 'Frozen Pizza', 'Frozen Fruit'}
     }
 
-  
-    # Testing things because I'm a dumbass
-    # test = 'Aisle 1'
-    # # test2 = aisleItems[test][random.choice(list(aisleItems[test]))]
-    # test3 = random.choice(list(aisleItems[test]))
-
-    # print(test)
-    # print(test3)
-
-    # print(aisleItems)
-
     numItems = random.randint(5, 10)
 
     index = 0
-    
+
     groceryList = []
 
     while index < numItems:
 
         aisleChoice = random.choice(list(aisleItems))
         item = random.choice(list(aisleItems[aisleChoice]))
-        groceryList.append(item)
-        index += 1
 
-    #########################
-    #   SEE GENAISLELIST    #
-    #########################
+        if item not in groceryList:
+            groceryList.append(item)
+            index += 1
 
-    # requiredAisles = []
-    # for reqItem in groceryList:
-    #     for aisle in aisleItems:
-    #        for item in aisleItems[aisle]:
-    #             if item == reqItem:
-    #                 if aisle not in requiredAisles:
-    #                     requiredAisles.append(aisle)
-    #                     continue
-
-    # print(len(aisleItems['Aisle 1'].intersection(groceryList)))
-
-    # print(genAisleList(groceryList, aisleItems))    
-    
 
     print()
     print("The items we need are:")
@@ -367,9 +253,121 @@ if __name__ == '__main__':
     print()
     print(aisleLayout.shoppingPath(aisles))
     print()
-    # print("The total cost was:")
-    # print()
-    # print()
 
     divider()
 
+    all = aisleLayout.printEdges()
+    optimal = aisleLayout.shoppingPath(aisles)
+
+    firstTry = []
+    frozenFirst = []
+    secondTry = []
+    thirdTry = []
+    frozenThird = []
+    cannotDo = []
+    moved = []
+    replacements = []
+
+    res = list(zip(optimal, optimal[1:] + optimal[:1]))
+    for item in res:
+        firstTry.append(list(item))
+
+    for item in all:
+        secondTry.append(item)
+
+    for item in firstTry:
+        if item in secondTry and item != ['Checkout', 'Entrance']:
+            thirdTry.append(item)
+
+    print(f"Paths that can be achieved: {thirdTry}")
+
+    for item in thirdTry:
+        if item in firstTry and item != ['Checkout', 'Entrance']:
+            firstTry.remove(item)
+
+    cannotDo = firstTry
+
+    print(f"Paths that cannot be achieved: {cannotDo}")
+
+    divider()
+
+
+    def pathGen(start, end, currentPath):
+
+        currentPath.append(start)
+
+        if start == end:
+            print(f"Path found: {currentPath}")
+            replacements.append(currentPath)
+            return
+
+        else:
+            neighbors = []
+            distances = []
+
+            for i in all:
+                if start in i:
+                    for x in i:
+                        if x is not start and x not in neighbors:
+                            neighbors.append(x)
+
+            for potential in neighbors:
+                for key in aisleLayout.dijkstra(potential):
+                    if key is potential:
+                        distances.append({key: aisleLayout.dijkstra(potential)[end] + aisleLayout.dijkstra(start)[potential]})
+
+            chosen = None
+            chosenDis = None
+
+            for pair in distances:
+
+                if chosen is None:
+                    chosen = list(pair.keys())[0]
+                    chosenDis = list(pair.values())[0]
+
+                elif int(list(pair.values())[0]) < chosenDis:
+                    chosen = list(pair.keys())[0]
+                    chosenDis = list(pair.values())[0]
+
+            pathGen(chosen, end, currentPath)
+
+
+    for neededPath in cannotDo:
+
+        print(f"Path bust be found between {neededPath[0]} and {neededPath[1]}")
+        pathGen(neededPath[0], neededPath[1], [])
+        divider()
+
+    shoppingPathClone = aisleLayout.shoppingPath(aisles)
+
+    res = list(zip(shoppingPathClone, shoppingPathClone[1:] + shoppingPathClone[:1]))
+
+    cannotDo = [tuple(x) for x in cannotDo]
+
+    for i in res:
+
+        if i in cannotDo:
+            next1 = replacements.pop(0)
+            res[res.index(i)] = next1
+
+    if ['Checkout', 'Entrance'] in res : res.remove(['Checkout', 'Entrance'])
+
+    print(f"Full Path: {res}")
+
+    cleanPath = []
+    
+    for val in res:
+        for inVal in val:
+            cleanPath.append(inVal)
+
+    print(f"Clean Path: {cleanPath}")
+
+    previous_value = None
+    finalPath = []
+
+    for elem in cleanPath:
+        if elem != previous_value:
+            finalPath.append(elem)
+            previous_value = elem
+
+    print(f"Final Path: {finalPath}")
